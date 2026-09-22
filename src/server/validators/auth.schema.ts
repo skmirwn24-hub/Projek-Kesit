b74d17707e53e24a7eb41893e290518297d6a75d
Dicky Ihsan Novanda <dickynovanda26@gmail.com>
2026-09-22 16:20:46 +0700

    faet(ganti kata)

--- src/server/validators/auth.schema.ts
+++ src/server/validators/auth.schema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  identifier: z.string().min(1, 'Email atau username wajib diisi'),
-  password: z.string().min(1, 'Password wajib diisi'),
+  identifier: z.string().min(1, 'Email atau username '),
+  password: z.string().min(1, 'Password '),
 });
 
 export type LoginInput = z.infer<typeof loginSchema>;
